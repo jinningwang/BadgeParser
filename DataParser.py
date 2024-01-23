104e5638c2e0ca88096a60087ce81d9663587c28
jinningwang <jinningwang>
2024-01-23 09:24:35 -0500

    Add ORCID bio and ResearchGate

--- DataParser.py
+++ DataParser.py
@@ -43,6 +43,59 @@ def scrape_google_scholar(profile_url):
         return None
 
 
+def fetch_researchgate_stats(profile_url):
+    """
+    Fetch ResearchGate statistics.
+
+    Parameters
+    ----------
+    profile_url : str
+        URL of the ResearchGate profile.
+
+    Returns
+    -------
+    dict
+        A dictionary containing ResearchGate statistics (e.g., followers, reads, citations).
+    """
+    try:
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+        }
+        response = requests.get(profile_url, headers=headers)
+        response.raise_for_status()  # Raise an exception for HTTP errors
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Error while fetching ResearchGate stats: {e}")
+        return None
+
+    # NOTE: skip, write the HTML to a file for debugging
+    # with open("data_researchgate.txt", "w", encoding='utf-8') as file:
+    #     file.write(soup.prettify())
+
+    # Replace 'your_element_id_or_class' with the actual ID or class
+    followers_element = soup.find('span', {'id': 'your_element_id_or_class'})
+    reads_element = soup.find('span', {'id': 'your_element_id_or_class'})
+    citations_element = soup.find('span', {'id': 'your_element_id_or_class'})
+
+    stats = {
+        'Followers': followers_element.text if followers_element else 'Not found',
+        'Reads': reads_element.text if reads_element else 'Not found',
+        'Citations': citations_element.text if citations_element else 'Not found'
+    }
+
+    if followers_element:
+        stats['Followers'] = followers_element.text.strip()
+
+    if reads_element:
+        stats['Reads'] = reads_element.text.strip()
+
+    if citations_element:
+        stats['Citations'] = citations_element.text.strip()
+    logging.info(f"ResearchGate: {stats}")
+    return stats
+
+
 def fetch_orcid_bio(orcid_id):
     """
     Fetch biography information from ORCID.
@@ -243,9 +296,10 @@ def generate_review_badge(journal_name, review_count, color="blue", logo=None, l
     return badge_markdown
 
 
-
-def generate_and_write_badges(citations,
-                              review_data,):
+def write_readme(citations,
+                 orcid_bio,
+                 review_data,
+                 researchgate_stats):
     """
     Generate and print the Google Scholar citation badge.
 
@@ -253,36 +307,68 @@ def generate_and_write_badges(citations,
     ----------
     citations : str
         The number of citations to display on the citation badge.
+    orcid_bio : str
+        Biography information from ORCID profile.
     review_data : dict
-        A dictionary with journal names as keys and review counts as values.
+        A dictionary with journal names as keys and review counts as values..
+    researchgate_stats : dict
+        A dictionary containing ResearchGate statistics (e.g., followers, reads, citations).
     """
+    # --- Google Scholar Citation Badge ---
     gscholar_link = "https://scholar.google.com/citations?user=Wr7nQZAAAAAJ&hl=en&oi=ao"
     gscholar_badge_markdown = generate_citation_badge(citations,
-                                             logo="googlescholar",
-                                             link=gscholar_link)
+                                                      logo="googlescholar",
+                                                      link=gscholar_link)
     logging.info(gscholar_badge_markdown)
 
+    # --- ORCID Peer Review Badges ---
     # Generate and print review badges for each journal
     for journal_name, review_count in review_data.items():
         review_badge_markdown = generate_review_badge(journal_name, review_count,
                                                       color="blue", logo="None", link=None)
         logging.info(review_badge_markdown)
 
+    # --- ResearchGate Statistics Badge ---
+    for stat_name, stat_value in researchgate_stats.items():
+        researchgate_badge_markdown = generate_citation_badge(stat_value,
+                                                              logo="researchgate",
+                                                              link=None)
+        msg = f"{stat_name}: {researchgate_badge_markdown}"
+        logging.info("Research Gate: " + msg)
+
+    # --- Write to README.md ---
     # BadgeParser
     readme_header = "# BadgeParser\nCreate customized badges.\n"
 
     # Write the badge Markdown code to Badge.md
     with open("README.md", "w") as badge_file:
+        # Write the header
         badge_file.write(readme_header)
 
-        badge_file.write("## Scholar\n")
+        # Write Google Scholar badge
+        badge_file.write("## Google Scholar\n")
         badge_file.write(gscholar_badge_markdown)
         badge_file.write("\n")
 
+        # Write ORCID Bio
+        badge_file.write("## ORCID Bio\n")
+        badge_file.write(orcid_bio)
+        badge_file.write("\n")
+
+        # Write ORCID Peer Review badges
         badge_file.write("## Peer Review\n")
         for journal_name, review_count in review_data.items():
-            review_badge_markdown = generate_review_badge(journal_name, review_count, color="blue", logo=None, link=None)
+            review_badge_markdown = generate_review_badge(
+                journal_name, review_count, color="blue", logo=None, link=None)
             badge_file.write(f"{review_badge_markdown}  ")
+        badge_file.write("\n")
+        # Write ResearchGate badges
+        badge_file.write("## ResearchGate\n")
+        for stat_name, stat_value in researchgate_stats.items():
+            researchgate_badge_markdown = generate_citation_badge(stat_value,
+                                                                  logo="researchgate",
+                                                                  link=None)
+
 
 def main():
     """
@@ -290,15 +376,22 @@ def main():
     """
     gs_profile_url = 'https://scholar.google.com/citations?user=Wr7nQZAAAAAJ&hl=en&oi=ao'
     orcid_id = '0000-0001-9302-3364'
+    researchgate_url = 'https://www.researchgate.net/profile/Jinning_Wang3'
 
+    # Fetch Google Scholar statistics
     gs_citations = scrape_google_scholar(gs_profile_url)
+
+    # Fetch ResearchGate statistics
+    researchgate_stats = fetch_researchgate_stats(researchgate_url)
+
+    # Fetch ORCID data
     orcid_bio = fetch_orcid_bio(orcid_id)
     orcid_total_review = fetch_orcid_total_review(orcid_id)
     review_data = fetch_orcid_review_data(orcid_id)
     publication_data = fetch_orcid_publication_data(orcid_id)
 
-    # Update the README.md file with the Google Scholar citation badge
-    generate_and_write_badges(gs_citations, review_data)
+    # Update the README.md file with the Google Scholar citation badge, review badges, and ResearchGate badges
+    write_readme(gs_citations, orcid_bio, review_data, researchgate_stats)
 
     scholar_data = {
         'gscite': gs_citations,
